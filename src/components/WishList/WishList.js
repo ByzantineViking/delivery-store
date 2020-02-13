e7255c812c1f7d3555227519a2847f8e1f29129e
saravit1 <teemu.saravirta@aalto.fi>
2020-02-13 12:18:23 +0200

    Fixed diffing issues by destructuring props and giving useEffect the correct dependencies

--- src/components/WishList/WishList.js
+++ src/components/WishList/WishList.js
@@ -3,40 +3,41 @@ import { connect } from 'react-redux'
 import { displayCollapsed, displayFull, displayNone } from '../../reducers/visibilityReducer'
 import './Wishlist.css'
 import ExpandedWishlist from './ExpandedWishlist'
-const WishList = (props) => {
+const Wishlist = (props) => {
+    const { wishlist, displayCollapsed, displayNone, ...nwProps } = props
     useEffect (() => {
-        if (props.wishlist.length > 0) {
-            props.displayCollapsed({ id: 'wishlist' })
+        if (wishlist.length > 0) {
+            displayCollapsed({ id: 'wishlist' })
         } else {
-            props.displayNone({ id: 'wishlist' })
+            displayNone({ id: 'wishlist' })
         }
-    }, [props.wishlist])
+    }, [wishlist, displayCollapsed, displayNone])
     const restaurantNames = () => {
         let holder = []
-        Object.keys(props.wishlist).reduce((acc, key) =>
-            holder = holder.concat(props.wishlist[key].name)
+        Object.keys(wishlist).reduce((acc, key) =>
+            holder = holder.concat(wishlist[key].name)
         , '')
         return holder
     }
     const handleClick = () => {
-        if(props.open.wishlist.collapsed) {
-            props.displayNone({ id: 'drawer1' })
-            props.displayNone({ id: 'drawer2' })
-            props.displayFull({ id: 'wishlist' })
+        if (nwProps.open.wishlist.collapsed) {
+            displayNone({ id: 'drawer1' })
+            displayNone({ id: 'drawer2' })
+            nwProps.displayFull({ id: 'wishlist' })
         }
     }
     return(
         <div>
-            {!props.open.wishlist.hidden && <div className="wishlist-container">
+            {!nwProps.open.wishlist.hidden && <div className="wishlist-container">
                 <h2>
                     <span role="img" aria-label="Delicious">
                         😋
                     </span>
                 </h2>
                 <div className="wishlist" onClick={handleClick}>
-                    {props.open.wishlist.full ?
+                    {nwProps.open.wishlist.full ?
                         <ExpandedWishlist /> :
-                        restaurantNames(props.wishlist).join()
+                        restaurantNames(wishlist).join()
                     }
                 </div>
             </div>}
@@ -54,4 +55,4 @@ const mapDispatchToProps = {
     displayCollapsed,
     displayNone
 }
-export default connect(mapStateToProps, mapDispatchToProps)(WishList)
+export default connect(mapStateToProps, mapDispatchToProps)(Wishlist)
